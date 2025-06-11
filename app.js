e70cbfbd958e2476d9572f2b67601cd9a9aeb1b1
Jigar Prajapati <jigarprajapati6457@gmail.com>
2025-06-11 22:30:54 +0530

    "/" route changes for deployment

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ async function main(){
     await mongoose.connect("mongodb://127.0.0.1:27017/AirBNB");
 }
 app.get("/",(req,res)=>{
-    res.send("home root");
+    res.redirect("/listings");
 });
 
 // server side validation for listing schema
