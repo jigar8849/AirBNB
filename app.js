d1229873db6d515b58eb2409d0e1354b0925a32a
Jigar Prajapati <jigarprajapati6457@gmail.com>
2025-06-17 14:40:22 +0530

    error pe error aa rahi hai routes folder ki vajha se

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("../AirBNB/models/listing")
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-const wrapAsync = require("./utils/wrapAsync");
 const ExpressError = require("./utils/ExpressError.js");
-const {listingSchema, reviewSchema} = require("./schema.js");  //require schema from joi for server side validation
-const Review = require("../AirBNB/models/review")
-
+const listings = require("./routes/listing.js"); // require all listings routes from routes folder 
+const reviews = require("./routes/review.js") //require all revirews router from routes folder
 
 app.set("view engine", "ejs");
 app.set("views",path.join(__dirname,"views"));
@@ -33,126 +30,29 @@ app.get("/",(req,res)=>{
     res.redirect("/listings");
 });
 
-// server side validation for listing schema
-const validateListing = (req,res,next)=>{
-    let {error} = listingSchema.validate(req.body);
-        if(error){
-            let errMsg = error.details.map((el)=> el.message).join(",");
-            throw new ExpressError(400, errMsg);
-        }else{
-            next()
-        }
-}
-
-const validateReview = (req,res,next)=>{
-    let {error} = reviewSchema.validate(req.body);
-        if(error){
-            let errMsg = error.details.map((el)=> el.message).join(",");
-            throw new ExpressError(400, errMsg);
-        }else{
-            next()
-        }
-}
 
 
-// Index Route
-app.get("/listings",wrapAsync(async(req,res)=>{
-    const allListings = await Listing.find({})
-    res.render("listings/index.ejs",{allListings});
-})
-)
-
-// New listing button 
-app.get("/listings/new",(req,res)=>{
-   res.render("listings/new.ejs"); 
-});
-
-// Show Route
-app.get("/listings/:id",wrapAsync(async(req,res)=>{
-    let {id} = req.params;
-    const listing = await Listing.findById(id).populate("reviews");
-    res.render("listings/show.ejs",{listing});
-})
-)
-
-// create new listing
-app.post("/listings",validateListing,wrapAsync(async(req,res,next)=>{
-        const  newListings = new Listing(req.body.listings);
-        await newListings.save();
-        res.redirect("/listings");
-    })
-);
-
-// Redirect to edit form
-app.get("/listings/:id/edit",wrapAsync(async(req,res)=>{
-    let {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs",{listing});
-}));
-
-app.put("/listings/:id",validateListing,wrapAsync(async(req,res)=>{
-      let {id} = req.params;
-      await Listing.findByIdAndUpdate(id, { ...req.body.listings });
-      res.redirect(`/listings/${id}`);
-}));
-
-
-// Delete route
-app.delete("/listings/:id",wrapAsync(async(req,res)=>{
-    let {id} = req.params;
-    await Listing.findByIdAndDelete(id);
-    res.redirect("/listings");
-}))
-
-// Reviews  - ka post route
-
-app.post("/listings/:id/reviews",validateReview, wrapAsync(async(req,res)=>{
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
-
-     listing.reviews.push(newReview);
-
-     await newReview.save();
-     await listing.save();
-
-     res.redirect(`/listings/${listing._id}`);
-}));
-
-
-
-// app.get("/testListing",(req,res)=>{
-//     let sampleListing = new Listing({
-//         title : "my home",
-//         description : "near the air-port",
-//         price : 1000,
-//         location : "Ahmedabad",
-//         country : "India"
-//     });
-//     sampleListing.save().then(()=>{
-//         console.log("data was saved")
-//         res.send("Testing was successed")
-//     })
-// })
-
-// this app.all give me error when i went to use it and still no any way to find to solve it 
-
+// this line use for use all route related to listings & reviews from routes folder
+app.use("/listings", listings)
+app.use("/listings/:id/reviews", reviews)
 
 app.get("/test-error", (req, res, next) => {
     next(new ExpressError(404, "This is a test error"));
 });
 
+app.listen(8080,()=>{
+    console.log("Server is running on port 8080");
+}) 
+
 
 app.use((err, req, res, next) => {
-    const { statusCode = 500 , message = "Something went wrong" } = err;
-    res.status(statusCode).render("error.ejs", { message }); // 🔁 this line must include `err`
+    const { statusCode = 500, message = "Something went wrong" } = err;
+    res.status(statusCode).render("error.ejs", { message }); // you can pass full `err` if needed
 });
 
 
-app.listen(8080,()=>{
-    console.log("Server is running on port 8080");
-})  
 
-
+// this app.all give me error when i went to use it and still no any way to find to solve it 
 // app.all("*",(req,res,next)=>{
 //     next(new ExpressError(404,"Page not found!!!"))    
 // })
---
+++ routes/listing.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const router = express.Router({mergeParams : true});
+const wrapAsync = require("../utils/wrapAsync.js");
+const { listingSchema } = require("../schema.js");  //require schema from joi for server side validation
+const ExpressError = require("../utils/ExpressError.js");
+const Listing = require("../models/listing.js")
+
+
+
+// server side validation for listing schema
+const validateListing = (req,res,next)=>{
+    let {error} = listingSchema.validate(req.body);
+        if(error){
+            let errMsg = error.details.map((el)=> el.message).join(",");
+            throw new ExpressError(400, errMsg);
+        }else{
+            next()
+        }
+}
+
+
+// Index Route
+router.get("/",wrapAsync(async(req,res)=>{
+    const allListings = await Listing.find({})
+    res.render("listings/index.ejs",{allListings});
+})
+)
+
+// New listing button 
+router.get("/new",(req,res)=>{
+   res.render("listings/new.ejs"); 
+});
+
+// Show Route
+router.get("/:id",wrapAsync(async(req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id).populate("reviews");
+    res.render("listings/show.ejs",{listing});
+})
+)
+
+
+
+
+
+// create new listing
+router.post("/",validateListing,wrapAsync(async(req,res,next)=>{
+        const  newListings = new Listing(req.body.listings);
+        // const newListings = new Listing(req.body);
+        await newListings.save();
+        res.redirect("/listings");
+    })
+);
+
+// Edit - Redirect to edit form
+router.get("/:id/edit",wrapAsync(async(req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs",{listing});
+}));
+
+// Update route
+router.put("/:id",validateListing,wrapAsync(async(req,res)=>{
+      let {id} = req.params;
+    //   await Listing.findByIdAndUpdate(id, { ...req.body.listings });
+    await Listing.findByIdAndUpdate(id, { ...req.body });
+      res.redirect(`/listings/${id}`);
+}));
+
+// Delete route
+router.delete("/:id",wrapAsync(async(req,res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+}))
+
+
+module.exports = router;
